4197904f3eaa9b93d3e8511da93950dfbcea3c6c
priyanshi072108-stack <priyanshi.072108@gmail.com>
2026-03-24 10:00:04 +0530

    Create dict2.py

---
+++ dict2.py
@@ -0,0 +1,12 @@
+# Dictionary of numbers
+data = {
+    "A": 25,
+    "B": 40,
+    "C": 15,
+    "D": 60
+}
+
+# Finding maximum value
+max_value = max(data.values())
+
+print("Maximum value in dictionary is:", max_value)
